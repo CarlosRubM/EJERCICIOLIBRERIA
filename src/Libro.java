539c338adfbcfe6f29b346149ed329cb2aee3805
CarlosRubM <carlos.rubio9@alu.uclm.es>
2023-04-20 10:17:09 +0200

    Clase libro

--- src/Libro.java
+++ src/Libro.java
@@ -3,8 +3,14 @@ public class Libro {
     private String titulo;
     private String autor;
     private int añoPublicacion;
+    private String ISBN;
+    private String genero;
 
-    private int año;
-
-
+    public Libro(String titulo, String autor, int añoPublicacion, String ISBN, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.ISBN = ISBN;
+        this.genero = genero;
+    }
 }
